cac28b2929bd370da6a0b801f6b95e668a732667
marrrcin <m.zablo@gmail.com>
2016-03-06 18:22:52 +0100

    ASP.NET Core 1.0 HTML Cache Example with Redis

---
+++ src/RedisCacheExample/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Configuration;
+using RedisCacheExample.Cache;
+
+namespace RedisCacheExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        protected IConfiguration Configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        
+        [Cache(Duration = 30)]
+        public IActionResult Index()
+        {
+            ViewBag.UsedCache = Convert.ToBoolean(Configuration["Cache:UseRedis"]) ? "Redis" : "In memory";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/RedisCacheExample/Startup.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Caching.Redis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RedisCacheExample.Cache;
+using RedisCacheExample.Models;
+using RedisCacheExample.Services;
+
+namespace RedisCacheExample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.marcin.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            //Cache config start
+            services.AddCaching();
+            var useRedis = Convert.ToBoolean(Configuration["Cache:UseRedis"]);
+            if (useRedis)
+            {
+                string redisConnection = Configuration["Redis:ConnectionString"],
+                       redisInstance = Configuration["Redis:InstanceName"];
+
+                services.AddSingleton<IDistributedCache>(factory =>
+                {
+                    var cache = new RedisCache(new RedisCacheOptions
+                    {
+                        Configuration = redisConnection,
+                        InstanceName = redisInstance
+                    });
+
+                    return cache;
+                });
+                services.AddSingleton<ICacheService, RedisCacheService>();
+            }
+            else
+            {
+                services.AddSingleton<IMemoryCache>(factory =>
+                {
+                    var cache =  new MemoryCache(new MemoryCacheOptions());
+                    return cache;
+                });
+                services.AddSingleton<ICacheService, MemoryCacheService>();
+            }
+
+
+            services.AddTransient<CacheAttribute>();
+            //Cache config end
+
+            services.AddInstance<IConfiguration>(Configuration);
+            services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+
+                // For more details on creating database during deployment see http://go.microsoft.com/fwlink/?LinkID=615859
+                try
+                {
+                    using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>()
+                        .CreateScope())
+                    {
+                        serviceScope.ServiceProvider.GetService<ApplicationDbContext>()
+                             .Database.Migrate();
+                    }
+                }
+                catch { }
+            }
+
+            app.UseIISPlatformHandler(options => options.AuthenticationDescriptions.Clear());
+
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            // To configure external authentication please see http://go.microsoft.com/fwlink/?LinkID=532715
+
+            app.UseMiddleware<CacheMiddleware>();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
